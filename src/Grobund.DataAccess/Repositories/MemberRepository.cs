d23df9e8f48ce97394ace13f853265a56a288589
RTNordkvist <rikkenordkvist@hotmail.com>
2022-11-22 11:27:54 +0100

    Initial projects and structure has been added

---
+++ src/Grobund.DataAccess/Repositories/MemberRepository.cs
@@ -0,0 +1,26 @@
+﻿using Grobund.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grobund.DataAccess.Repositories
+{
+    public class MemberRepository
+    {
+        private readonly string connectionString;
+
+        public MemberRepository(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public int AddMember(Member member)
+        {
+            // TO DO: Insert code block to save new member in database
+
+            return member.Id;
+        }
+    }
+}
