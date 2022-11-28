7c1889aff0d783dcd358c8e0704c17f2c2b314f6
Tera-Nuovo <tera41930@edu.ucl.dk>
2022-11-28 19:10:56 +0900

    added table and stored procedure

---
+++ src/GrobundDB/dbo/StoredProcedures/spMembers_Insert.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE dbo.spMembers_Insert
+	@FirstName nvarchar(30),
+	@LastName nvarchar(50),
+	@Email nvarchar(50),
+	@PhoneNumber nvarchar(20),
+	@id int = 0 OUTPUT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	INSERT INTO dbo.Members (FirstName, LastName, Email, PhoneNumber)
+	VALUES (@FirstName, @LastName, @Email, @PhoneNumber);
+
+	SELECT @id = SCOPE_IDENTITY();
+END
---
+++ src/GrobundDB/dbo/Tables/Members.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Members] (
+    [Id]          INT        IDENTITY (1, 1) NOT NULL,
+    [FirstName]   NCHAR (30) NOT NULL,
+    [LastName]    NCHAR (50) NULL,
+    [Email]       NCHAR (50) NULL,
+    [PhoneNumber] NCHAR (20) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+)
